d48a5127992a5834309a56e7c2ef70921cb4a254
manoajk <manoajk@yahoo.com>
2017-11-18 02:09:14 -0500

    Modified script

--- python_scripts/playoff_picture.py
+++ python_scripts/playoff_picture.py
@@ -1,128 +1,174 @@
 import pandas as pd
 import numpy as np
 
-# '../data_files/'
-
 regular_season_games = 13
 total_teams = 12
 playoff_teams = 6
+playoff_bye_teams = 8 - playoff_teams
+
+standings_file = 'standings.txt'
 
 non_playoff = total_teams - playoff_teams
 
-# teams = {}
-# with open("standings.txt") as standings_file:
-#     rank = 1
+symbols = {}
+playoff_clinch_string = "Wins to Clinch Playoffs"
+symbols[playoff_clinch_string] = "x"
 
-#     for line in standings_file:
-#         splitter = line.replace("\n", "").split("\t")
-#         if not splitter[0].__contains__('TEAM') and len(splitter) > 1:
-#             team_and_owners = splitter[0].split('(')
-#             points_for = float(splitter[1])
-#             points_against = float(splitter[2])
-#             record = splitter[5]
-#             record_list = [int(num) for num in record.split('-')]
-#             streak = splitter[6]
+bye_clinch_string = "Wins to Clinch First-Round Bye"
+symbols[bye_clinch_string] = "z"
 
-#             team = team_and_owners[0]
-#             owners = team_and_owners[1].replace(')', "")
+homefield_clinch_string = "Wins to Clinch Homefield Advantage"
+symbols[homefield_clinch_string] = "*"
 
-#             win = record_list[0]
-#             losses = record_list[1]
-#             ties = record_list[2]
-#             games_played = win + losses + ties
-#             pct = (win + 0.5 * ties) / games_played
-# #             max_win = win + (regular_season_games - games_played)
+elim_string = 'Losses till Playoff-Ineligbile'
+symbols[elim_string] = "e"
 
-#             team_info = {}
-#             team_info['Rank'] = rank
-#             team_info['Team Name'] = team
-#             team_info['Owners'] = owners
-#             team_info['Points For'] = points_for
-#             team_info['Points Against'] = points_against
-#             team_info['Record'] = record
-#             team_info['Wins'] = win
-#             team_info['Games Played'] = games_played
-#             team_info['Pct (%)'] = pct
-# #             team_info['Max Wins'] = max_win
 
-#             # print(max_wins)
-# #             if win in wins:
-# #                 wins[win] += 1
-# #             else:
-# #                 wins[win] = 1
-
-# #             if max_win in max_wins:
-# #                 max_wins[max_win] += 1
-# #             else:
-# #                 max_wins[max_win] = 1
-
-#             teams[rank] = team_info
-#             rank += 1
-
-# df = pd.DataFrame.from_dict(teams).transpose()
-# # print("Wins Filter", np.sum(df['Max Wins'] > 7))
-# # print("Games Played", df.get('Games Played'))
-# # print("ONe row", df.loc[[2]])
-# # for index, row in df.iterrows():
-# #     print(row['Team Name'])
-# # games_left = regular_season_games - df['Games Played']
-# # df['Clinched'] = df['Wins'] > (df.iloc[playoff_teams]['Wins'] + games_left)
-# # df['Eliminated'] = (df['Wins'] + games_left) < df.iloc[playoff_teams - 1]['Wins']
-
-columns=['Rank', 'Team Name', 'Owners', 'Points For',
+def parse_standings():
+    columns=['Rank', 'Team Name', 'Owners', 'Points For',
          'Points Against', 'Record', 'Wins', 'Games Played', 'Pct (%)']
-df = pd.DataFrame(columns=columns)
+    dataframe = pd.DataFrame(columns=columns)
 
-with open("../data_files/standings.txt") as s_file:
-    rank = 1
+    with open('../data_files/' + standings_file) as s_file:
+        rank = 1
 
-    for line in s_file:
-        splitter = line.replace("\n", "").split("\t")
-        if not splitter[0].__contains__('TEAM') and len(splitter) > 1:
-            team_and_owners = splitter[0].split('(')
-            points_for = float(splitter[1])
-            points_against = float(splitter[2])
-            record = splitter[5]
-            record_list = [int(num) for num in record.split('-')]
-            streak = splitter[6]
+        for line in s_file:
+            splitter = line.replace("\n", "").split("\t")
+            if not splitter[0].__contains__('TEAM') and len(splitter) > 1:
+                team_and_owners = splitter[0].split('(')
+                points_for = float(splitter[1])
+                points_against = float(splitter[2])
+                record = splitter[5]
+                record_list = [int(num) for num in record.split('-')]
+                streak = splitter[6]
 
-            team = team_and_owners[0]
-            owners = team_and_owners[1].replace(')', "")
+                team = team_and_owners[0]
+                owners = team_and_owners[1].replace(')', "")
 
-            win = record_list[0]
-            losses = record_list[1]
-            ties = record_list[2]
-            games_played = win + losses + ties
-            pct = (win + 0.5 * ties) / games_played
-            
-            df.loc[rank] = [rank, team, owners, points_for, points_against, record, win, games_played, pct]
-            rank += 1
+                win = record_list[0]
+                losses = record_list[1]
+                ties = record_list[2]
+                games_played = win + losses + ties
+                pct = (win + 0.5 * ties) / games_played
 
-df.Rank = df.Rank.astype(int)
+                dataframe.loc[rank] = [rank, team, owners, points_for, points_against, record, win, games_played, pct]
+                rank += 1
 
-games_left = regular_season_games - df['Games Played']
-df['Wins for Clinching'] = (df.iloc[playoff_teams]['Wins'] + games_left) - df['Wins'] + 1
-df['Losses for Elimination'] = (df['Wins'] + games_left) - df.iloc[playoff_teams - 1]['Wins']
+    return dataframe
 
-to_clinch = df[df['Wins for Clinching'] == 1]
+def team_loss_string(row):
+    output = row['Team Name']
+    output += " ("
+    output += row["Record"]
+    output += ") Loss"
+    return output
+    
+def string_for_team_losses(filtered_df):
+    output = []
 
-fringe = df[df["Wins"] == df.iloc[playoff_teams]['Wins']]
+    length = len(filtered_df)
+    rnge = filtered_df["Rank"].max() - filtered_df["Rank"].min() + 1
 
-outside_length = len(fringe)
+    if (length == 0): return output
+    if (length == 1): return [team_loss_string(filtered_df.iloc[0])]
+    keyword = length
+    if length == rnge:
+        keyword = 'BOTH' if length == 2 else 'ALL'
+    output += [keyword + " of the following need(s) to occur:"]
+    for index, row in filtered_df.iterrows():
+        output += [" - " + team_loss_string(row)]
 
-fringe_teams = fringe["Rank"].max() - fringe["Rank"].min() + 1
+    return output
 
-# if outside_length == fringe_teams:
-#     criteria = "ALL"
-# else:
-#     criteria = "Choose " + str(outside_length) + " out of " + str(fringe_teams)
-criteria = "ALL" if outside_length == fringe_teams else "Choose " + str(outside_length) + " out of " + str(fringe_teams)
 
-losses_for_clinch = [criteria + " of the following need to occur:"]
-for index, row in fringe.iterrows():
-    losses_for_clinch.append(" - " + row['Team Name'] + " (" + row["Record"] + ") Loss")
-# losses_for_clinch = "\n".join(losses_for_clinch)
-# print(losses_for_clinch)
+
+def keep_teams_below(df, safezone_num, column_string):
+    # if df == None or safezone_num < 1 or safezone_num > len(df) or column_string == None or column_string not in df: return ''
+
+    games_left = regular_season_games - df['Games Played']
+    df[column_string] = (df.iloc[safezone_num]['Wins'] + games_left) - df['Wins'] + 1
+
+    fringe = df[df["Wins"] == df.iloc[safezone_num]['Wins']]
+    outside_length = len(fringe)
+    fringe_teams = fringe["Rank"].max() - fringe["Rank"].min() + 1
+
+    return string_for_team_losses(fringe)
+
+def catchup_to_teams_above(df, safezone_num, column_string):
+    # if df == None or safezone_num < 1 or safezone_num > len(df) or column_string == None or column_string not in df: return ''
+
+    games_left = regular_season_games - df['Games Played']
+    df[column_string] = (df['Wins'] + games_left) - df.iloc[safezone_num - 1]['Wins'] + 1
+    
+    barely_in = df[df["Wins"] == df.iloc[safezone_num - 1]['Wins']]
+    barely_in_length = len(barely_in)
+    barely_in_teams = barely_in["Rank"].max() - barely_in["Rank"].min() + 1
+
+    return string_for_team_losses(barely_in)
+
+def set_status(df, symbols):
+    # if df != None and symbols != None:
+    if True:
+        df['Status'] = ''
+        for key in symbols:
+            if key in df.columns:
+                df.loc[df[key] == 0, 'Status'] += symbols[key]
+        df.loc[df['Status'] != '', 'Status'] = '[' + df['Status'] + ']'
+
+
+def new_tab_lines(num_tabs=0, num_lines=1):
+    return num_lines * '\n' + num_tabs * '\t'
+
+
+df = parse_standings()
+
+playoff_clinch_other_teams = keep_teams_below(df, playoff_teams, playoff_clinch_string)
+bye_clinch_other_teams = keep_teams_below(df, playoff_bye_teams, bye_clinch_string)
+homefield_clinch_other_teams = keep_teams_below(df, 1, homefield_clinch_string)
+
+non_elimination = catchup_to_teams_above(df, playoff_teams, elim_string)
+
+set_status(df, symbols)
+
+
+def criteria_builder(criteria_string, display_title, string_arr):
+    # print("STRING ARR", string_arr)
+    output = []
+    and_or = None
+    if row[criteria_string] == 1:
+        and_or = "OR"
+    elif row[criteria_string] == 2:
+        and_or = "AND"
+    if and_or != None:
+#         space = " "*3
+        two_tabs = new_tab_lines(2)
+        one_tab = new_tab_lines(1)
+        # output.append(two_tabs)
+        # output.append(display_title)
+        # output.append(one_tab)
+        # output.append("   a) Win")
+        # output.append(two_tabs)
+        # output.append(and_or)
+        # output.append(one_tab)
+        # output.append("   b) ")
+        # output.append(two_tabs.join(string_arr))
+
+        output += two_tabs
+        output += display_title
+        output += " -"
+        # output += "Clinching Scenarios -"
+        output += one_tab
+        output += "   a) Win"
+        output += two_tabs
+        output += and_or
+        output += one_tab
+        output += "   b) "
+        print("STRING ARR", string_arr)
+        output += two_tabs.join(string_arr)
+
+    return output
+
+
 
 output = []
 output.append("Without further ado,\n" \
@@ -132,26 +178,40 @@ output.append("Without further ado,\n" \
             + "[x] Clinched Playoff Berth\n" \
             + "[e] Eliminated From Playoff Contention\n\n")
 for index, row in df.iterrows():
-    if row['Wins for Clinching'] == 0:
-        output.append("[x]")
-	# potentially put this in dataframe itself?
-	# for symbol_string in clinch_symbols:
-	# 	if row[symbol_string] <= 0:
-	# 		output.append(clinch_symbols[symbol_string])
-    # if row['Wins to Clinch Playoffs'] == 0:
-    #     output.append("[x]")
-    output.append("\t" + str(row['Rank']) + ") [" + row['Record'] + "] "+ row['Team Name'] + " (" + row['Owners'] + ")")
-    and_or = None
-    if row['Wins for Clinching'] == 1:
-        and_or = "OR"
-    elif row['Wins for Clinching'] == 2:
-        and_or = "AND"
-    if and_or != None:
-#         space = " "*3
-        output.append("\n\t\tClinching Scenarios -\n\t   a) Win\n\t\t")
-        output.append(and_or)
-        output.append("\n\t   b) ")
-        output.append("\n\t\t".join(losses_for_clinch))
-    output.append("\n\n")
+
+    output += row['Status']
+    output += "\t"
+    output += str(int(row['Rank']))
+    output +=  ") ["
+    output += row['Record']
+    output += "] "
+    output += row['Team Name']
+    output += " ("
+    output += row['Owners']
+    output += ")"
+
+    output += criteria_builder(playoff_clinch_string, 'Clinching Scenarios', playoff_clinch_other_teams)
+    output += criteria_builder(elim_string, 'Preventing Elimination', non_elimination)
+#   and_or = None
+#   if row['Wins to Clinch Playoffs'] == 1:
+#       and_or = "OR"
+#   elif row['Wins to Clinch Playoffs'] == 2:
+#       and_or = "AND"
+#   if and_or != None:
+# #         space = " "*3
+#       two_tabs = new_tab_lines(2)
+#       one_tab = new_tab_lines(1)
+
+#       output += two_tabs
+#       output += "Clinching Scenarios -"
+#       output += one_tab
+#       output += "   a) Win"
+#       output += two_tabs
+#       output += and_or
+#       output += one_tab
+#       output += "   b) "
+#       output += two_tabs.join(playoff_clinch_other_teams)
+
+    output += "\n\n"
 output = "".join(output)
 print(output)
