360abbdf0fd3a06c4044c3f90408c551c13a5047
manoajk <manoajk@yahoo.com>
2017-11-18 03:00:25 -0500

    More functionally-driven

--- python_scripts/playoff_picture.py
+++ python_scripts/playoff_picture.py
@@ -1,30 +1,7 @@
 import pandas as pd
 import numpy as np
 
-regular_season_games = 13
-total_teams = 12
-playoff_teams = 6
-playoff_bye_teams = 8 - playoff_teams
-
-standings_file = 'standings.txt'
-
-non_playoff = total_teams - playoff_teams
-
-symbols = {}
-playoff_clinch_string = "Wins to Clinch Playoffs"
-symbols[playoff_clinch_string] = "x"
-
-bye_clinch_string = "Wins to Clinch First-Round Bye"
-symbols[bye_clinch_string] = "z"
-
-homefield_clinch_string = "Wins to Clinch Homefield Advantage"
-symbols[homefield_clinch_string] = "*"
-
-elim_string = 'Losses till Playoff-Ineligbile'
-symbols[elim_string] = "e"
-
-
-def parse_standings():
+def parse_standings(standings_file):
     columns=['Rank', 'Team Name', 'Owners', 'Points For',
          'Points Against', 'Record', 'Wins', 'Games Played', 'Pct (%)']
     dataframe = pd.DataFrame(columns=columns)
@@ -61,11 +38,11 @@ def team_loss_string(row):
     output += " ("
     output += row["Record"]
     output += ") Loss"
+
     return output
     
 def string_for_team_losses(filtered_df):
     output = []
-
     length = len(filtered_df)
     rnge = filtered_df["Rank"].max() - filtered_df["Rank"].min() + 1
 
@@ -83,8 +60,8 @@ def string_for_team_losses(filtered_df):
 
 
 def keep_teams_below(df, safezone_num, column_string):
-    # if df == None or safezone_num < 1 or safezone_num > len(df) or column_string == None or column_string not in df: return ''
-
+    if type(df) == None or safezone_num < 1 or safezone_num > len(df) or type(column_string) == None:# or column_string not in df.columns:
+            return ''
     games_left = regular_season_games - df['Games Played']
     df[column_string] = (df.iloc[safezone_num]['Wins'] + games_left) - df['Wins'] + 1
 
@@ -107,111 +84,91 @@ def catchup_to_teams_above(df, safezone_num, column_string):
     return string_for_team_losses(barely_in)
 
 def set_status(df, symbols):
-    # if df != None and symbols != None:
-    if True:
-        df['Status'] = ''
-        for key in symbols:
-            if key in df.columns:
-                df.loc[df[key] == 0, 'Status'] += symbols[key]
-        df.loc[df['Status'] != '', 'Status'] = '[' + df['Status'] + ']'
+    scenarios = []
+    df['Status'] = ''
 
+    for key in symbols:
+        if symbols[key][1]: scenarios.append((key, symbols[key][3], keep_teams_below(df, symbols[key][2], key)))
+        else: scenarios.append((key, symbols[key][3], catchup_to_teams_above(df, symbols[key][2], key)))
+
+        if key in df.columns: df.loc[df[key] == 0, 'Status'] += symbols[key][0]
+    df.loc[df['Status'] != '', 'Status'] = '[' + df['Status'] + ']'
+
+    return scenarios
 
 def new_tab_lines(num_tabs=0, num_lines=1):
     return num_lines * '\n' + num_tabs * '\t'
 
-
-df = parse_standings()
-
-playoff_clinch_other_teams = keep_teams_below(df, playoff_teams, playoff_clinch_string)
-bye_clinch_other_teams = keep_teams_below(df, playoff_bye_teams, bye_clinch_string)
-homefield_clinch_other_teams = keep_teams_below(df, 1, homefield_clinch_string)
-
-non_elimination = catchup_to_teams_above(df, playoff_teams, elim_string)
-
-set_status(df, symbols)
-
-
-def criteria_builder(criteria_string, display_title, string_arr):
-    # print("STRING ARR", string_arr)
+def criteria_builder(row, criteria_string, display_title, string_arr):
     output = []
     and_or = None
-    if row[criteria_string] == 1:
-        and_or = "OR"
-    elif row[criteria_string] == 2:
-        and_or = "AND"
+
+    if row[criteria_string] == 1: and_or = "OR"
+    elif row[criteria_string] == 2: and_or = "AND"
     if and_or != None:
-#         space = " "*3
         two_tabs = new_tab_lines(2)
         one_tab = new_tab_lines(1)
-        # output.append(two_tabs)
-        # output.append(display_title)
-        # output.append(one_tab)
-        # output.append("   a) Win")
-        # output.append(two_tabs)
-        # output.append(and_or)
-        # output.append(one_tab)
-        # output.append("   b) ")
-        # output.append(two_tabs.join(string_arr))
 
         output += two_tabs
         output += display_title
         output += " -"
-        # output += "Clinching Scenarios -"
         output += one_tab
         output += "   a) Win"
         output += two_tabs
         output += and_or
         output += one_tab
         output += "   b) "
-        print("STRING ARR", string_arr)
         output += two_tabs.join(string_arr)
 
     return output
 
+def base_symbols(playoff_teams, playoff_bye_teams):
+    clinch_symbols = {}
 
+    clinch_symbols['Wins to Clinch Playoffs'] = ["x", True, playoff_teams, "Playoff Clinching Scenarios"]
+    clinch_symbols['Wins to Clinch First-Round Bye'] = ["z", True, playoff_bye_teams, "Bye-Week Clinching Scenarios"]
+    clinch_symbols['Wins to Clinch Homefield Advantage'] = ["*", True, 1, "Homefield Advantage Clinching Scenarios"]
+    clinch_symbols['Losses till Playoff-Ineligbile'] = ["e", False, playoff_teams, "Playoff Elimination Prevention Scenarios"]
 
-output = []
-output.append("Without further ado,\n" \
-            + "PLAYOFF PICTURE brought to you by a Julin’ Around Manjie\n" \
-            + "[*] Clinched Homefield Advantage in the Playoffs\n" \
-            + "[z] Clinched First-Round Bye\n" \
-            + "[x] Clinched Playoff Berth\n" \
-            + "[e] Eliminated From Playoff Contention\n\n")
-for index, row in df.iterrows():
+    return clinch_symbols
 
-    output += row['Status']
-    output += "\t"
-    output += str(int(row['Rank']))
-    output +=  ") ["
-    output += row['Record']
-    output += "] "
-    output += row['Team Name']
-    output += " ("
-    output += row['Owners']
-    output += ")"
+def main(standings_file, regular_season_games, total_teams, playoff_teams, playoff_bye_teams, symbols):
+    df = parse_standings(standings_file)
+    non_playoff = total_teams - playoff_teams
+    scenarios = set_status(df, symbols)
 
-    output += criteria_builder(playoff_clinch_string, 'Clinching Scenarios', playoff_clinch_other_teams)
-    output += criteria_builder(elim_string, 'Preventing Elimination', non_elimination)
-#   and_or = None
-#   if row['Wins to Clinch Playoffs'] == 1:
-#       and_or = "OR"
-#   elif row['Wins to Clinch Playoffs'] == 2:
-#       and_or = "AND"
-#   if and_or != None:
-# #         space = " "*3
-#       two_tabs = new_tab_lines(2)
-#       one_tab = new_tab_lines(1)
+    output = []
+    output.append("Without further ado,\n" \
+                + "PLAYOFF PICTURE brought to you by a Julin’ Around Manjie\n" \
+                + "[*] Clinched Homefield Advantage in the Playoffs\n" \
+                + "[z] Clinched First-Round Bye\n" \
+                + "[x] Clinched Playoff Berth\n" \
+                + "[e] Eliminated From Playoff Contention\n\n")
+    for index, row in df.iterrows():
+        output += row['Status']
+        output += "\t"
+        output += str(int(row['Rank']))
+        output +=  ") ["
+        output += row['Record']
+        output += "] "
+        output += row['Team Name']
+        output += " ("
+        output += row['Owners']
+        output += ")"
+        for scenario in scenarios:
+            output += criteria_builder(row, scenario[0], scenario[1], scenario[2])
+        output += "\n\n"
+    return "".join(output)
 
-#       output += two_tabs
-#       output += "Clinching Scenarios -"
-#       output += one_tab
-#       output += "   a) Win"
-#       output += two_tabs
-#       output += and_or
-#       output += one_tab
-#       output += "   b) "
-#       output += two_tabs.join(playoff_clinch_other_teams)
+######
 
-    output += "\n\n"
-output = "".join(output)
-print(output)
+regular_season_games = 13
+total_teams = 12
+playoff_teams = 6
+playoff_bye_teams = 8 - playoff_teams
+s_file = 'standings.txt'
+
+symbols = base_symbols(playoff_teams, playoff_bye_teams)
+print_output = main(s_file, regular_season_games, total_teams, playoff_teams, playoff_bye_teams, symbols)
+
+print(print_output)
